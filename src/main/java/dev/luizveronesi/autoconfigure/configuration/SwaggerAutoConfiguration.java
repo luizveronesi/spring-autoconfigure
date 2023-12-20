4bb023acdd328e0cf3440beba91660a8fc3d9bda
Luiz Veronesi <luiz@texugo.com.br>
2023-12-19 22:40:26 -0300

    just adding a link

--- src/main/java/dev/luizveronesi/autoconfigure/configuration/SwaggerAutoConfiguration.java
+++ src/main/java/dev/luizveronesi/autoconfigure/configuration/SwaggerAutoConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// https://stackoverflow.com/questions/61193993/authentication-with-google-in-web-api-and-swagger
+
 @Configuration
 @EnableWebMvc
 public class SwaggerAutoConfiguration implements WebMvcConfigurer {
